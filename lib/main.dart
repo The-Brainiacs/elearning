f3dc6140c4057491127819e8a400433c72d73b55
Muhammad Huzaifah Bin Azman <m.huzaifah1998@gmail.com>
2020-03-29 22:42:11 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
+<<<<<<< Updated upstream
 class App extends StatelessWidget {
 
   
+=======
+class App extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<App> {
+
+  final FirebaseMessaging _messaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _messaging.getToken().then((token) {
+      print(token);
+    });
+  }
+>>>>>>> Stashed changes
 
  @override
  Widget build(BuildContext context) {
