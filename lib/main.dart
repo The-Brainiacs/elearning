641c34a2d5ce344f0caa62630290bfc4ed3cc05f
azrin1998 <azrin1998@users.noreply.github.com>
2020-03-31 15:39:51 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -24,41 +24,6 @@ class _MyAppState extends State<App> {
       print(token);
     });
   }
-<<<<<<< HEAD
-}
-
-Widget _myListView(BuildContext context) {
-  return ListView(
-    children: <Widget>[
-      ListTile(
-        title: Text('Dashboard'),
-        leading: Icon(Icons.folder, color: Color(0xff5c001e)),
-      ),
-      ListTile(
-        title: Text('Profile'),
-        leading: Icon(Icons.person, color: Color(0xff5c001e)),
-      ),
-      ListTile(
-        title: Text('Messages'),
-        leading: Icon(Icons.
-        message, color: Color(0xff5c001e)),
-      ),
-      ListTile(
-        title: Text('Notification'),
-        leading: Icon(Icons.notifications, color: Color(0xff5c001e)),
-      ),
-      ListTile(
-        title: Text('Private Files'),
-        leading: Icon(Icons.lock, color: Color(0xff5c001e)),
-      ),
-      ListTile(
-        title: Text('Calendar'),
-        leading: Icon(Icons.calendar_today, color: Color(0xff5c001e)),
-      ),
-      ListTile(
-        title: Text('Archive'),
-        leading: Icon(Icons.archive, color: Color(0xff5c001e)),
-=======
   
  @override
  Widget build(BuildContext context) {
@@ -68,7 +33,6 @@ Widget _myListView(BuildContext context) {
       theme: ThemeData(
         primaryColor: Color(0xff5c001e),
         fontFamily: 'Arial',
->>>>>>> 75a5540437f542baf0a91d0aaade9dae8335bfe7
       ),
     home: LoginPage(),
    );
