b431e4a45955f4592f95bbb3b94900f3881d6a56
Muhammad Huzaifah Bin Azman <m.huzaifah1998@gmail.com>
2020-04-02 18:35:40 +0800

    Update profile.dart

--- lib/ui/views/profile.dart
+++ lib/ui/views/profile.dart
@@ -20,31 +20,227 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/images/profile_picture.jpg'),
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Color(0xff5c001e),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'Matric Number',
-              style: TextStyle(
-                fontSize: 17.0,
-                color: Color(0xff5c001e),
-              ),
-            ),
+            buildProfileInfoContainer(),
+            buildCoursesContainer(),
+            buildDetailsContainer(),
           ]
         )
       ),
       bottomNavigationBar: buildBottomNav(),
     );
   }
+}
+
+Container buildProfileInfoContainer() {
+  return Container(
+    width: 360,
+    padding: EdgeInsets.all(10.0),
+    margin: EdgeInsets.symmetric(
+      vertical: 5.0,
+      horizontal: 20.0,
+    ),
+    child: Align(
+      child: Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/images/profile_picture.jpg'),
+          ),
+          Text(
+            'Muhammad Aiman',
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color(0xff5c001e),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'A17CS0000',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Color(0xff5c001e),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+            width: 300,
+            child: Divider(
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Container buildCoursesContainer() {
+  return Container( // Courses
+    width: 360,
+    padding: EdgeInsets.all(10.0),
+    margin: EdgeInsets.symmetric(
+      vertical: 10.0,
+      horizontal: 25.0,
+    ),
+    decoration: BoxDecoration(
+      color: Color(0xffEEE9EA),
+      border: Border.all(
+        color: Color(0xff5c001e),
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(5.0),
+      )
+    ),
+    child: Align(
+      alignment: Alignment.centerLeft,
+      child: buildStudentCourses(),
+    ),
+  );
+}
+
+Container buildDetailsContainer() {
+  return Container( // Details
+    width: 360,
+    padding: EdgeInsets.all(10.0),
+    margin: EdgeInsets.symmetric(
+      vertical: 10.0,
+      horizontal: 25.0,
+    ),
+    decoration: BoxDecoration(
+      color: Color(0xffEEE9EA),
+      border: Border.all(
+        color: Color(0xff5c001e),
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(5.0),
+      )
+    ),
+    child: buildStudentDetails(),
+  );
+}
+
+Column buildStudentCourses() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+
+          Text(
+            'Courses', 
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 23.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+          Text(
+            '+4 more', 
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 5,
+        width: 300,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Software Engineering Project 1 (SCSJ3032-01)', 
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+          SizedBox(
+            height: 5,
+            width: 300,
+          ),
+          Text(
+            'Theory Of Computer Science (SCSJ3203-05)', 
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Column buildStudentDetails() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'Details', 
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 23.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+          Text(
+            '+3 more', 
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 5,
+        width: 300,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'User Details', 
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+          SizedBox(
+            height: 5,
+            width: 300,
+          ),
+          Text(
+            'Privacy And Policy', 
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+          SizedBox(
+            height: 5,
+            width: 300,
+          ),
+          Text(
+            'Reports', 
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Color(0xff5c001e)
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
 }
 
 BottomNavigationBar buildBottomNav() {
@@ -73,39 +269,5 @@ BottomNavigationBar buildBottomNav() {
             title: Text('Log Out', style: TextStyle(color: Colors.white),),
           ),
         ],
-  );
-}
-
-Column buildBody() {
-  return Column(children: <Widget>[
-    Row( // User's default picture, name & matric id
-      children: [
-        Container(
-          color: Color(0xEEE9EA),
-          margin: EdgeInsets.all(25.0),
-          // child: FlutterLogo(size: 60.0,),
-          //nama
-          //matrik
-        ),
-        // Container(),
-      ],
-    ),
-    Row( // Courses
-      children: [
-        Container(
-          color: Colors.black,
-          margin: EdgeInsets.all(25.0),
-          // child: FlutterLogo(size: 60.0,),
-        ),
-        // Container(),
-      ],
-    ),
-    Row( // Details
-      children: [
-        Container(),
-        // Container(),
-      ],
-    ),
-  ]
   );
 }
