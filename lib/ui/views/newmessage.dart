29d1f2c7d6b49763b20cb10cf9ddc59275b39453
AsmaSalsabilaYunos <asmasalsabilamy@gmail.com>
2020-05-05 01:25:14 +0800

    style new message screen

--- lib/ui/views/newmessage.dart
+++ lib/ui/views/newmessage.dart
@@ -1,4 +1,3 @@
-import 'package:elearning_app/ui/views/messages.dart';
 import 'package:flutter/material.dart';
 
 class NewMessagePage extends StatefulWidget {
@@ -9,8 +8,7 @@ class NewMessagePage extends StatefulWidget {
 }
 
 class _NewMessagePageState extends State<NewMessagePage> {
-
-int counter = 0;
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,91 +16,136 @@ int counter = 0;
       appBar: AppBar(
         title: Text('New Messages'),
       ),
-       body: Center(child: BodyLayout(), ),
+      body: Center(
+        child: BodyLayout(),
+      ),
+      
       bottomNavigationBar: buildBottomNav(),
     );
   }
 }
 
 class BodyLayout extends StatefulWidget {
-  
   @override
   _BodyLayoutState createState() => _BodyLayoutState();
-  
 }
 
 class _BodyLayoutState extends State<BodyLayout> {
-   final TextEditingController _controller = new TextEditingController();
-   final TextEditingController _controller2 = new TextEditingController();
+  final TextEditingController _controller = new TextEditingController();
+  final TextEditingController _controller2 = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-    body: new Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        
-        new Text('Send to'),
-        new TextField(
-            controller: _controller,
-            decoration: new InputDecoration(
-                
-                hintText: 'example@gmail.com',
-            ),    
-        ),
-        new Text('Messages'),
-        new TextField(
-            controller: _controller2,
-            decoration: new InputDecoration(
-                hintText: 'Your messages',
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 40),
+          new Text(
+            'Recipient : ',
+            style: TextStyle(
+              fontSize: 20,
             ),
-            
-        ),
-        new RaisedButton(
+          ),
+          SizedBox(height: 10),
+          new TextField(
+            controller: _controller,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.lightBlue[50],
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        hintText: "example@gmail.com",
+                        errorText: "*required",
+                        errorStyle: TextStyle(
+                            color: Colors.red, fontWeight: FontWeight.bold)),
+                  ),
+              SizedBox(height: 20),
+          new Text(
+            'Messages : ',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 10),
+          new TextField(
+              controller: _controller2,
+              decoration: InputDecoration(
+    filled: true,
+                      fillColor: Colors.lightBlue[50],
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: "Your messages",
+              )),
+          SizedBox(height: 30),
+          new RaisedButton(
             onPressed: () {
               showDialog(
-                  context: context,
-                  child: new AlertDialog(
-                      title: new Text('Send messages?'),
-                      content: new Text(_controller.text),
-                  ),
+                context: context,
+                child: new AlertDialog(
+                  title: new Text('Send messages to '),
+                  content: new Text(_controller.text),
+                ),
               );
             },
             child: new Text('SEND'),
-        ),
-      ],
-    ),
-  );
+          ),
+        ],
+      ),
+    );
   }
 }
 
+
+
 BottomNavigationBar buildBottomNav() {
   return BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Color(0xff5c001e),
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today, color: Colors.white),
-            title: Text('', style: TextStyle(color: Colors.white),),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications,
-
-             color: Colors.white),
-            title: Text('', style: TextStyle(color: Colors.white),),
-
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.folder, color: Colors.white),
-            title: Text('', style: TextStyle(color: Colors.white),),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.mail, color: Colors.white),
-            title: Text('', style: TextStyle(color: Colors.white),),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.exit_to_app, color: Colors.white),
-            title: Text('', style: TextStyle(color: Colors.white),),
-          ),
-        ],
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: Color(0xff5c001e),
+    items: [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.calendar_today, color: Colors.white),
+        title: Text(
+          '',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.notifications, color: Colors.white),
+        title: Text(
+          '',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.folder, color: Colors.white),
+        title: Text(
+          '',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.mail, color: Colors.white),
+        title: Text(
+          '',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.exit_to_app, color: Colors.white),
+        title: Text(
+          '',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ],
   );
 }
