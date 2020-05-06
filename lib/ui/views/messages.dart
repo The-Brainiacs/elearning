66fe8119dcf09c6642852fe267845d4bbad3c5ea
AsmaSalsabilaYunos <asmasalsabilamy@gmail.com>
2020-05-07 04:24:27 +0800

    combine new message page into message page

--- lib/ui/views/messages.dart
+++ lib/ui/views/messages.dart
@@ -1,3 +1,4 @@
+import 'package:elearning_app/ui/models/message_data.dart';
 import 'package:elearning_app/ui/views/newmessage.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,8 @@ MessagesPage(this._msg);
 }
 
 class _MessagesPageState extends State<MessagesPage> {
-
+  final TextEditingController _controller = new TextEditingController();
+  final TextEditingController _controller2 = new TextEditingController();
 int counter = 0;
 
   @override
@@ -66,13 +68,121 @@ int counter = 0;
           color: Colors.grey,
         ),
       ),
-       floatingActionButton: FloatingActionButton.extended(
-         label: const Text('Add New Message'),
-              icon: const Icon(Icons.add),
-              heroTag: null,
-              onPressed: () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => NewMessagePage())),
+      //  floatingActionButton: FloatingActionButton.extended(
+      //    label: const Text('Add New Message'),
+      //         icon: const Icon(Icons.add),
+      //         heroTag: null,
+      //         onPressed: () => Navigator.push(
+      //       context, MaterialPageRoute(builder: (context) => NewMessagePage())),
+      //       ),
+      floatingActionButton: FloatingActionButton.extended(
+            label: const Text('Send message'),
+            heroTag: null,
+            onPressed: () {
+              showDialog(
+                context: context,
+                child: Column(
+                  children: <Widget>[
+                    new AlertDialog(
+                      title: 
+                          Column(
+                            children: <Widget>[
+          SizedBox(height: 40),
+          new Text(
+            'Recipient : ',
+            style: TextStyle(
+              fontSize: 20,
             ),
+          ),
+          SizedBox(height: 10),
+          new TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.lightBlue[50],
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: "example@gmail.com",
+                errorText: "*required",
+                errorStyle:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+          ),
+          SizedBox(height: 20),
+          new Text(
+            'Messages : ',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 10),
+          new TextField(
+              controller: _controller2,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.lightBlue[50],
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: "Your messages",
+              )),
+          SizedBox(height: 30),
+          FloatingActionButton.extended(
+            label: const Text('Send message'),
+            heroTag: null,
+            onPressed: () {
+              showDialog(
+                context: context,
+                child: Column(
+                  children: <Widget>[
+                    new AlertDialog(
+                      title: 
+                          Column(
+                            children: <Widget>[
+                              new Text('Send messages to '),
+                              Text( _controller.text, 
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                ),),
+                              new Text('----------------------------------------'),
+                            ],
+                          ),
+                      content: 
+                          new Text(_controller2.text),
+                    ),
+                    FloatingActionButton.extended(
+            label: const Text('Ok'),
+            icon: const Icon(Icons.check_circle),
+            heroTag: null,
+            onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MessagesPage(mockData))),
+            
+          ),
+                  ],
+                ),
+              );
+            },
+            
+          ),
+        ],
+                          ),
+                      
+                    ),
+                   
+                  ],
+                ),
+              );
+            },
+            //   () => Navigator.push(
+            // context, MaterialPageRoute(builder: (context) => MessagesPage(mockData))),
+          ),
+            
       bottomNavigationBar: buildBottomNav(),
     );
   }
