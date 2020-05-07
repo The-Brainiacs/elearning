ce47626a5572f897bbf5833ecc1532beb44a18d0
Nursyahira Sabrina <nursyahiras12@gmail.com>
2020-05-07 23:28:05 +0800

    updated calendar.dart

--- lib/ui/views/calendar.dart
+++ lib/ui/views/calendar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 //import 'package:firebase/firebase.dart';
 
-
 class CalendarPage extends StatefulWidget {
   @override
   _CalendarPageState createState() => _CalendarPageState();
@@ -12,12 +11,10 @@ class _CalendarPageState extends State<CalendarPage> {
   CalendarController _controller;
   Map<DateTime, List<dynamic>> _events;
 
-
   @override
   void initState() {
     super.initState();
     _controller = CalendarController();
-    
   }
 
   Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
@@ -46,91 +43,96 @@ class _CalendarPageState extends State<CalendarPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TableCalendar(
-              events: _events,
-              initialCalendarFormat: CalendarFormat.week,
-              calendarStyle: CalendarStyle(
-                  todayColor: Colors.orange,
-                  selectedColor: Theme.of(context).primaryColor,
-                  todayStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white)),
-              headerStyle: HeaderStyle(
-                centerHeaderTitle: true,
-                formatButtonDecoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                formatButtonTextStyle: TextStyle(color: Colors.white),
-                formatButtonShowsNext: false,
-              ),
-              startingDayOfWeek: StartingDayOfWeek.monday,
-              onDaySelected: (date, events) {
-                print(date.toIso8601String());
-              },
-              builders: CalendarBuilders(
-                selectedDayBuilder: (context, date, events) => Container(
-                    margin: const EdgeInsets.all(4.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Text(
-                      date.day.toString(),
-                      style: TextStyle(color: Colors.white),
-                    )),
-                todayDayBuilder: (context, date, events) => Container(
-                    margin: const EdgeInsets.all(4.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Text(
-                      date.day.toString(),
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ),
-              calendarController: _controller,
-            )
+            buildTableCalendar(context)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Colors.orange,
+        label: Text('Add Event'),
+        icon: Icon(Icons.check_circle),
         onPressed: () {},
       ),
       bottomNavigationBar: buildBottomNav(),
     );
   }
 
-BottomNavigationBar buildBottomNav() {
-  return BottomNavigationBar(
-    type: BottomNavigationBarType.fixed,
-    backgroundColor: Color(0xff5c001e),
-    items: [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.calendar_today, color: Colors.white),
-        title: Text(''),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.notifications, color: Colors.white),
-        title: Text(''),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.folder, color: Colors.white),
-        title: Text(''),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.mail, color: Colors.white),
-        title: Text(''),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.exit_to_app, color: Colors.white),
-        title: Text(''),
-      ),
-    ],
-  );
-}
+  TableCalendar buildTableCalendar(BuildContext context) {
+    return TableCalendar(
+            events: _events,
+            initialCalendarFormat: CalendarFormat.week,
+            calendarStyle: CalendarStyle(
+                todayColor: Colors.orange,
+                selectedColor: Theme.of(context).primaryColor,
+                todayStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                    color: Colors.white)),
+            headerStyle: HeaderStyle(
+              centerHeaderTitle: true,
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              formatButtonTextStyle: TextStyle(color: Colors.white),
+              formatButtonShowsNext: false,
+            ),
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            onDaySelected: (date, events) {
+              print(date.toIso8601String());
+            },
+            builders: CalendarBuilders(
+              selectedDayBuilder: (context, date, events) => Container(
+                  margin: const EdgeInsets.all(4.0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Text(
+                    date.day.toString(),
+                    style: TextStyle(color: Colors.white),
+                  )),
+              todayDayBuilder: (context, date, events) => Container(
+                  margin: const EdgeInsets.all(4.0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Text(
+                    date.day.toString(),
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+            calendarController: _controller,
+          );
+  }
+
+  BottomNavigationBar buildBottomNav() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Color(0xff5c001e),
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today, color: Colors.white),
+          title: Text(''),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications, color: Colors.white),
+          title: Text(''),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.folder, color: Colors.white),
+          title: Text(''),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.mail, color: Colors.white),
+          title: Text(''),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.exit_to_app, color: Colors.white),
+          title: Text(''),
+        ),
+      ],
+    );
+  }
 }
