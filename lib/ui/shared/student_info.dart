07fd31c918cd4435ddd379b56a8d3c54685d5917
Huzaifah and Hakim <m.huzaifah1998@gmail.com>
2020-05-09 14:31:38 +0800

    Delete student_info.dart

--- lib/ui/shared/student_info.dart
+++
@@ -1,9 +0,0 @@
-class Student {
-  String email = 'm.huzaifah@mail';
-  String phone = '999';
-
-  Student({this.email, this.phone});
-
-  Student.copy(Student from) : this(email: from.email, phone: from.phone);
-
-}
