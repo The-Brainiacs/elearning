e662cfbc831db8762c09cd4fd759e35476f5d372
AsmaSalsabilaYunos <asmasalsabilamy@gmail.com>
2020-07-21 10:56:12 +0800

    ..

--- lib/ui/views/messages.dart
+++ lib/ui/views/messages.dart
@@ -20,215 +20,220 @@ class _MessagesPageState extends State<MessagesPage> {
 
   int counter = 0;
 
+  
   @override
   Widget build(BuildContext context) {
+    return buildMainScreen(context);
+  }
+
+  Scaffold buildMainScreen(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Messages'),
-        actions: <Widget>[
-          Icon(Icons.message, color: Colors.white),
-        ],
-      ),
-      body: new Column(
-        children: <Widget>[
-          SizedBox(
-            height: 30,
-            width: 350,
-          ),
-          new Expanded(
-            child: new ListView.builder(
-                itemCount: ctrl1.length,
-                itemBuilder: (BuildContext ctxt, int iindex) {
-                  final item = ctrl1[iindex];
-                  return Column(
-                    children: <Widget>[
-                      Dismissible(
-                        key: Key(item),
-                        onDismissed: (direction) {
-                          setState(() {
-                            ctrl1.removeAt(iindex);
-                            ctrl2.removeAt(iindex);
-                          });
-                        },
-                        background: Container(color: Colors.red),
-                        child: Row(
-                          children: <Widget>[
-                            new Icon(Icons.person),
-                      new Text(
-                        '  ' + ctrl1[iindex]  + '  ',
-                        style: TextStyle(
-                          fontSize: 21.0,
-                          color: Color(0xff5c001e),
-                          fontWeight: FontWeight.bold,
-                        ),
+    appBar: AppBar(
+      title: Text('Messages'),
+      actions: <Widget>[
+        Icon(Icons.message, color: Colors.white),
+      ],
+    ),
+    body: new Column(
+      children: <Widget>[
+        SizedBox(
+          height: 30,
+          width: 350,
+        ),
+        new Expanded(
+          child: new ListView.builder(
+              itemCount: ctrl1.length,
+              itemBuilder: (BuildContext ctxt, int iindex) {
+                final item = ctrl1[iindex];
+                return Column(
+                  children: <Widget>[
+                    Dismissible(
+                      key: Key(item),
+                      onDismissed: (direction) {
+                        setState(() {
+                          ctrl1.removeAt(iindex);
+                          ctrl2.removeAt(iindex);
+                        });
+                      },
+                      background: Container(color: Colors.red),
+                      child: Row(
+                        children: <Widget>[
+                          new Icon(Icons.person),
+                    new Text(
+                      '  ' + ctrl1[iindex]  + '  ',
+                      style: TextStyle(
+                        fontSize: 21.0,
+                        color: Color(0xff5c001e),
+                        fontWeight: FontWeight.bold,
                       ),
-                      new Text(
-                        ctrl2[iindex],
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.black,
-                          fontStyle: FontStyle.italic,
-                        ),
+                    ),
+                    new Text(
+                      ctrl2[iindex],
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
                       ),
-                          ],
-                        ),
+                    ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 30,
-                        width: 350,
-                        child: Divider(
-                          color: Colors.black,
-                        ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                      width: 350,
+                      child: Divider(
+                        color: Colors.black,
                       ),
-                    ],
-                  );
-                }),
-                
-          ),
-        ],
-      ),
-      // body: ListView.separated(
-      //   itemCount: widget._msg.length,
-      //   itemBuilder: (context, index) => ListTile(
-      //     title: Text(widget._msg[index].email),
-      //     subtitle: Text(widget._msg[index].content),
-      //   ),
-      //   separatorBuilder: (context, index) => Divider(
-      //     color: Colors.grey,
-      //   ),
-      // ),
+                    ),
+                  ],
+                );
+              }),
+              
+        ),
+      ],
+    ),
+    // body: ListView.separated(
+    //   itemCount: widget._msg.length,
+    //   itemBuilder: (context, index) => ListTile(
+    //     title: Text(widget._msg[index].email),
+    //     subtitle: Text(widget._msg[index].content),
+    //   ),
+    //   separatorBuilder: (context, index) => Divider(
+    //     color: Colors.grey,
+    //   ),
+    // ),
 
-      floatingActionButton: FloatingActionButton.extended(
-        label: const Text('Add new message'),
-        heroTag: null,
-        onPressed: () {
-          showDialog(
-            context: context,
-            child: Column(
-              children: <Widget>[
-                new AlertDialog(
-                  title: Column(
-                    children: <Widget>[
-                      SizedBox(height: 40),
-                      new Text(
-                        'Recipient : ',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
+    floatingActionButton: FloatingActionButton.extended(
+      label: const Text('Add new message'),
+      heroTag: null,
+      onPressed: () {
+        showDialog(
+          context: context,
+          child: Column(
+            children: <Widget>[
+              new AlertDialog(
+                title: Column(
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    new Text(
+                      'Recipient : ',
+                      style: TextStyle(
+                        fontSize: 20,
                       ),
-                      SizedBox(height: 10),
-                      new TextField(
-                        
-                        controller: _controller,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.person),
-                            filled: true,
-                            fillColor: Colors.lightBlue[50],
-                            errorBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.black,
-                              ),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            hintText: "example : Misha Omar",
-                            errorText: "*required",
-                            errorStyle: TextStyle(
-                                color: Colors.red,
-                                fontWeight: FontWeight.bold)),
-                        onSubmitted: (text) {
-                          ctrl1.add(text);
-                          
-                          setState(() {});
-                        },
-                        
-                      ),
-                      SizedBox(height: 20),
-                      new Text(
-                        'Messages : ',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      new TextField(
-
-                        // maxLines: 4,
-                        controller: _controller2,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.message),
+                    ),
+                    SizedBox(height: 10),
+                    new TextField(
+                      
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
                           filled: true,
                           fillColor: Colors.lightBlue[50],
-                          enabledBorder: OutlineInputBorder(
+                          errorBorder: OutlineInputBorder(
                             borderSide: BorderSide(
                               color: Colors.black,
                             ),
                             borderRadius: BorderRadius.circular(10.0),
                           ),
-                          hintText: "Your messages",
-                        ),
-                        onSubmitted: (text) {
-                          ctrl2.add(text);
-                          _controller.clear();
-                          _controller2.clear();
-                          setState(() {});
-                        },
+                          hintText: "example : Misha Omar",
+                          errorText: "*required",
+                          errorStyle: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold)),
+                      onSubmitted: (text) {
+                        ctrl1.add(text);
+                        
+                        setState(() {});
+                      },
+                      
+                    ),
+                    SizedBox(height: 20),
+                    new Text(
+                      'Messages : ',
+                      style: TextStyle(
+                        fontSize: 20,
                       ),
-                      SizedBox(height: 30),
-                      FloatingActionButton.extended(
-                        label: const Text('Send'),
-                        heroTag: null,
-                        onPressed: () => Navigator.of(context).pop(),
-                        // onPressed: () {
-                        //   showDialog(
-                        //     context: context,
-                        //     child: Column(
-                        //       children: <Widget>[
-                        //         new AlertDialog(
-                        //           title: Column(
-                        //             children: <Widget>[
-                        //               new Text('Send messages to '),
-                        //               Text(
-                        //                 _controller.text,
-                        //                 style: TextStyle(
-                        //                   fontStyle: FontStyle.italic,
-                        //                 ),
-                        //               ),
-                        //               new Text(
-                        //                   '----------------------------------------'),
-                        //             ],
-                        //           ),
-                        //           content: new Text(_controller2.text),
-                        //         ),
-                        //         FloatingActionButton.extended(
-                        //           label: const Text('Ok'),
-                        //           icon: const Icon(Icons.check_circle),
-                        //           heroTag: null,
-                        //           onPressed: () => Navigator.push(
-                        //               context,
-                        //               MaterialPageRoute(
-                        //                   builder: (context) =>
-                        //                       MessagesPage(mockData))),
-                        //           // Navigator.maybePop(context),
-                        //           // Navigator.of(context).pushAndRemoveUntil(
-                        //           //   MaterialPageRoute(builder: (context) =>MessagesPage(mockData)), ModalRoute.withName('settings')),
-                        //           // Navigator.popUntil(context, ModalRoute.withName('messages')),
-                        //         ),
-                        //       ],
-                        //     ),
-                        //   );
-                        // },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+                    ),
+                    SizedBox(height: 10),
+                    new TextField(
 
-      bottomNavigationBar: buildBottomNav(),
-    );
+                      // maxLines: 4,
+                      controller: _controller2,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.message),
+                        filled: true,
+                        fillColor: Colors.lightBlue[50],
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        hintText: "Your messages",
+                      ),
+                      onSubmitted: (text) {
+                        ctrl2.add(text);
+                        _controller.clear();
+                        _controller2.clear();
+                        setState(() {});
+                      },
+                    ),
+                    SizedBox(height: 30),
+                    FloatingActionButton.extended(
+                      label: const Text('Send'),
+                      heroTag: null,
+                      onPressed: () => Navigator.of(context).pop(),
+                      // onPressed: () {
+                      //   showDialog(
+                      //     context: context,
+                      //     child: Column(
+                      //       children: <Widget>[
+                      //         new AlertDialog(
+                      //           title: Column(
+                      //             children: <Widget>[
+                      //               new Text('Send messages to '),
+                      //               Text(
+                      //                 _controller.text,
+                      //                 style: TextStyle(
+                      //                   fontStyle: FontStyle.italic,
+                      //                 ),
+                      //               ),
+                      //               new Text(
+                      //                   '----------------------------------------'),
+                      //             ],
+                      //           ),
+                      //           content: new Text(_controller2.text),
+                      //         ),
+                      //         FloatingActionButton.extended(
+                      //           label: const Text('Ok'),
+                      //           icon: const Icon(Icons.check_circle),
+                      //           heroTag: null,
+                      //           onPressed: () => Navigator.push(
+                      //               context,
+                      //               MaterialPageRoute(
+                      //                   builder: (context) =>
+                      //                       MessagesPage(mockData))),
+                      //           // Navigator.maybePop(context),
+                      //           // Navigator.of(context).pushAndRemoveUntil(
+                      //           //   MaterialPageRoute(builder: (context) =>MessagesPage(mockData)), ModalRoute.withName('settings')),
+                      //           // Navigator.popUntil(context, ModalRoute.withName('messages')),
+                      //         ),
+                      //       ],
+                      //     ),
+                      //   );
+                      // },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    ),
+
+    bottomNavigationBar: buildBottomNav(),
+  );
   }
 }
 
